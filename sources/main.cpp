5b315a3d563b617a4cea9cc837d152c01a6b9061
I T <68477469+ktoEtoTakoy@users.noreply.github.com>
2022-12-03 15:46:38 +0100

    typos fixes

--- sources/main.cpp
+++ sources/main.cpp
@@ -45,7 +45,7 @@ void execute(task todo_task)
             tasklist.push({RELOAD_ISK, sim_time+60, todo_task.pid, 0});
             tasklist.push({FIRST_PART, sim_time+(dist_to_target-5)/2.1, todo_task.pid, 0});
             tasklist.push({DETECT_TARGET, sim_time+detect_time, todo_task.pid, 0});
-            cout << "Time: "<< sim_time << ". Launced iskander. Left: "<< iskander_amm <<"\n";
+            cout << "Time: "<< sim_time << ". Launched iskander. Left: "<< iskander_amm <<"\n";
             break;
         }
         case FIRST_PART:
@@ -244,7 +244,7 @@ int main(int argc, char *argv[])
     cout << "------------------------------------------------------------\n"
 		 << "                 RESAULTS OF SIMULATION\n"
 		 << "------------------------------------------------------------\n"
-		 << "Target hit: " << target_hits << "\n"
+		 << "Target hits: " << target_hits << "\n"
 		 << "Downed missles: " << downed_missiles << "\n"
 		 << "------------------------------------------------------------\n"
 		<< endl;
